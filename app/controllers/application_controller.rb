439fd46dce3b8c1a6494a0aa9eba3106bddf49f3
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-02 22:48:13 +1100

    Add a set_profile stub in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # :nodoc:
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+    def set_profile!
+      redirect_to new_profile_path if user_signed_in? and current_user.profile.nil?
+    end
 end
