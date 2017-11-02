9657b99e41a9cf317838efbbf47ad0e407112d4b
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-02 22:49:14 +1100

    Update routes and add a root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :profiles, only: %i[index show new create edit update destroy]
+  resources :profiles, only: %i[show new create edit update destroy]
   resources :products
   resources :photos
+  resources :shopping_carts, only: %i[index create destroy]
+
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
