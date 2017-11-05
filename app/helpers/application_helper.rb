390610ff1e5eca9d03de66dcac1453684405ef55
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-06 01:28:42 +1100

    Add helper method for retrieving search location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # :nodoc:
 module ApplicationHelper
+  # Search Location Default. If blank, default to Melbourne
+  def search_location
+    user_signed_in? ? current_user.profile.billing_address.full_address : 'Melbourne, VIC, Australia'
+  end
 end
