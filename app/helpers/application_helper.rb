481a36a90fd71d17bdd20c1574e2030272617ab1
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-06 13:18:33 +1100

    Add styling to products listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
   def search_location
     user_signed_in? ? current_user.profile.billing_address.full_address : 'Melbourne, VIC, Australia'
   end
+
+  # Formatted style for background images
+  def style_for_backgound_image(url)
+    "background-image: url(#{url});"
+  end
 end
