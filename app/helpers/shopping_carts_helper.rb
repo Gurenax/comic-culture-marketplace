343e231e8a8ee2f6bfa3c1d28b6173f92a0bfc3b
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-07 12:01:00 +1100

    Add styling to shopping cart #index

--- app/helpers/shopping_carts_helper.rb
+++ app/helpers/shopping_carts_helper.rb
@@ -1,5 +1,11 @@
 module ShoppingCartsHelper
+  # Format amount from centts
   def format_amount_from_cents(amount)
     (amount / 100.0).round(2)
+  end
+
+  # Format weight to kg from g
+  def format_grams_to_kg(weight)
+    weight / 1000.0
   end
 end
